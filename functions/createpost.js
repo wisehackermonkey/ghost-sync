18c09f710aa887856bc678c26f9fd8c4291e4a4f
Oran C <oranbusiness@gmail.com>
2021-11-16 16:09:20 -0800

    [add] error checking to createpost, cleaned up reponse

--- functions/createpost.js
+++ functions/createpost.js
@@ -1,21 +1,55 @@
 const GhostAdminAPI = require('@tryghost/admin-api');
- 
-exports.handler = async function(event, context) {
+
+exports.handler = async function (event, context) {
     console.log("Ghost Sync /api/v1/createpost.js:")
     let query = event.queryStringParameters
-    let {baseUrl,ghostAdminApiKey,ghostContentApiKey,image,public,version} = event.queryStringParameters
+    let { baseUrl, ghostAdminApiKey, ghostContentApiKey, title, image, public, version } = event.queryStringParameters
     console.log(query.ghostAdminApiKey)
     // Configure the client
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    };
+    if (ghostAdminApiKey === undefined || ghostAdminApiKey === "") {
+        return {
+            statusCode: 401,
+            headers: headers,
+            body: JSON.stringify({ error: "missing ghostAdminApiKey" })
+        };
+    }
+    if (ghostAdminApiKey.match(/^([a-f0-9]{24}:[a-f0-9]{64})/gm) === null) {
+        return {
+            statusCode: 401,
+            headers: headers,
+            body: JSON.stringify({ error: "ghostAdminApiKey: must follow format <24 characters>:<64 characters>" })
+        };
+    }
+    if (title === undefined || title === "") {
+        return {
+            statusCode: 406,
+            headers: headers,
+            body: JSON.stringify({ error: "missing title <string>, example 'title': 'Ghost sync: the amazing obsidian app'" })
+        }
+    }
+
+    if (baseUrl === undefined || baseUrl === "") {
+        return {
+            statusCode: 406,
+            headers: headers,
+            body: JSON.stringify({ error: "missing baseUrl <string>, example 'baseUrl': 'https://oransblog.com'" })
+        }
+    }
     const api = new GhostAdminAPI({
         url: baseUrl,
         // Admin API key goes here
-        key: ghostAdminApiKey,
+        key: ghostAdminApiKey.trim(),
         version: 'v3'
     });
-    
+
     let postData = {
         // "version": version,
-        "title": query.title,
+        "title": title,
         // "url": "https://oran.ghost.io/ghost-sync-test-post/",
         // "public": Boolean(public),
         // "image": image
@@ -25,35 +59,31 @@ exports.handler = async function(event, context) {
     // console.log(results)
 
     let {
-id, 
-title, 
-mobiledoc, 
-feature_image, 
-featured, 
-status, 
-visibility, 
-created_at, 
-updated_at
-} = results
+        id,
+        _title,
+        mobiledoc,
+        feature_image,
+        featured,
+        status,
+        visibility,
+        created_at,
+        updated_at
+    } = results
 
-let response = {
-    "version": "1.0",
-    "id":id, 
-    "title":title, 
-    "mobiledoc":mobiledoc, 
-    "feature_image":feature_image, 
-    "featured":featured, 
-    "status":status, 
-    "visibility":visibility, 
-    "created_at":created_at, 
-    "updated_at":updated_at,
-}
-console.log(response)
-const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-  };
+    let response = {
+        "version": "1.0",
+        "id": id,
+        "title": _title,
+        "mobiledoc": mobiledoc,
+        "feature_image": feature_image,
+        "featured": featured,
+        "status": status,
+        "visibility": visibility,
+        "created_at": created_at,
+        "updated_at": updated_at,
+    }
+    console.log(response)
+
     return {
         statusCode: 200,
         headers: headers,
